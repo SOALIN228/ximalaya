e8cb367bd53bbb9c1754c54c97e41b1e389382ea
lml <soalin228@163.com>
2020-07-15 08:13:10 +0800

    feat: init project

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+/**
+ * User: soalin
+ * Date: 2020/7/14
+ * Time: 23:45
+ * Desc:
+ */
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Index = () => {
+  return (
+    <View>
+      <Text>123</Text>
+    </View>
+  );
+};
+
+export default Index;
